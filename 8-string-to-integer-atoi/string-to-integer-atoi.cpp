a261d6aecbb0f23ef797119505db1b021ced47a7
Dikshasonawane15 <diksha1sonawane06@gmail.com>
2026-08-15 22:00:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (82.31%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.length();
+
+        // Skip leading spaces
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+
+        // Check sign
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        long long result = 0;
+
+        // Process digits
+        while (i < n && isdigit(s[i])) {
+            result = result * 10 + (s[i] - '0');
+
+            // Handle overflow
+            if (sign * result > INT_MAX) {
+                return INT_MAX;
+            }
+            if (sign * result < INT_MIN) {
+                return INT_MIN;
+            }
+
+            i++;
+        }
+
+        return static_cast<int>(sign * result);
+    }
+};
