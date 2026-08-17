9050f70463793b0aec514d328fa5dfe34a853943
Dikshasonawane15 <diksha1sonawane06@gmail.com>
2026-08-17 22:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (56.57%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZeroFoundAt = 0;
+        
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        
+        // Fill the remaining elements with zero
+        for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
